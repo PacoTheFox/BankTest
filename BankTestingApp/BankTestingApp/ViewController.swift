37c7d3a908025d280c391ec7b25b9e262200f22e
PacoTheFox <franaguisan011@gmail.com>
2025-02-04 15:16:50 -0600

    Git Ignore commit

---
+++ BankTestingApp/BankTestingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankTestingApp
+//
+//  Created by Francisco Aguirre San Román on 04/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
