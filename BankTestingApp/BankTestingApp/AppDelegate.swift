8d01972e9db9364d72730fae7fc19463a62a2336
PacoTheFox <franaguisan011@gmail.com>
2025-02-04 23:21:21 -0600

    feat: Add TableView

--- BankTestingApp/BankTestingApp/AppDelegate.swift
+++ BankTestingApp/BankTestingApp/AppDelegate.swift
@@ -28,7 +28,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         onboardingContainerViewController.delegate = self
         dummyViewController.logoutDelegate = self
         
-        window?.rootViewController = mainViewController
+       // window?.rootViewController = mainViewController
+        window?.rootViewController = AccountSummaryViewController()
         
         mainViewController.selectedIndex = 1
         return true
