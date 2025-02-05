edca9fbfc92053da153ee34c2e6a74d59d33422b
PacoTheFox <franaguisan011@gmail.com>
2025-02-04 21:27:15 -0600

    Show onboarding only one

--- BankTestingApp/BankTestingApp/AppDelegate.swift
+++ BankTestingApp/BankTestingApp/AppDelegate.swift
@@ -9,22 +9,63 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
+    
+    let loginViewController = LoginViewController()
+    let onboardingContainerViewController = OnboardingContainerViewController()
+    let dummyViewController = DummyViewController()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
-       // window?.rootViewController = LoginViewController()
-        window?.rootViewController = OnboardingContainerViewController()
+        
+        loginViewController.delegate = self
+        onboardingContainerViewController.delegate = self
+        dummyViewController.logoutDelegate = self
+        
+        window?.rootViewController = loginViewController
         
         return true
     }
     
-
-
 }
 
+extension AppDelegate: LoginViewControllerDelegate {
+    func didLogin() {
+        if LocalState.hasOnboarded {
+            setRootViewController(dummyViewController)
+            return
+        } else {
+            setRootViewController(onboardingContainerViewController)
+        }
+    }
+}
 
+extension AppDelegate: OnboardingContainerViewControllerDelegate {
+    func didFinishOnboarding() {
+        setRootViewController(dummyViewController)
+    }
+}
+
+extension AppDelegate: LogoutDelegate {
+    func didLogout() {
+        LocalState.hasOnboarded = true
+        setRootViewController(loginViewController)
+    }
+}
+
+extension AppDelegate {
+    func setRootViewController(_ vc: UIViewController, animated: Bool = true) {
+        guard animated, let window = self.window else {
+            self.window?.rootViewController = vc
+            self.window?.makeKeyAndVisible()
+            return
+        }
+        
+        window.rootViewController = vc
+        window.makeKeyAndVisible()
+        UIView.transition(with: window, duration: 0.3, options: .transitionCrossDissolve, animations: nil, completion: nil)
+    }
+}
--- BankTestingApp/BankTestingApp/Login/LoginViewController.swift
+++ BankTestingApp/BankTestingApp/Login/LoginViewController.swift
@@ -7,6 +7,14 @@
 
 import UIKit
 
+protocol LogoutDelegate: AnyObject {
+    func didLogout()
+}
+
+protocol LoginViewControllerDelegate: AnyObject {
+    func didLogin()
+}
+
 class LoginViewController: UIViewController {
     
     let titleLabel = UILabel()
@@ -14,7 +22,9 @@ class LoginViewController: UIViewController {
     let loginView = LoginView()
     let signInButton = UIButton(type: .system)
     let errorMessageLabel = UILabel()
-
+    
+    weak var delegate: LoginViewControllerDelegate?
+    
     var username: String? {
         return loginView.usernameTextField.text
     }
@@ -27,6 +37,11 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
         style()
         layout()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        signInButton.configuration?.showsActivityIndicator = false
     }
 
 }
@@ -127,8 +142,9 @@ extension LoginViewController {
         }
         
         
-        if username == "Paco" && password == "Welcome" {
+        if username == " " && password == " " {
             signInButton.configuration?.showsActivityIndicator = true
+            delegate?.didLogin()
         } else {
             configureView(withMessage: "Incorrect username or password!")
         }
